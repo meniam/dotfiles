87ca1ed68743d2c3add002db4f6a2a424f818567
Eugene Myazin <eugene.myazin@gmail.com>
2026-08-01 21:59:20 +0300

    feat(nvim): add gitsigns hunk navigation and staging keybindings
    ]c/[c jump between hunks; <leader>hs/hr/hp/hb stage, reset, preview,
    and blame the current hunk/line.

--- modules/nvim/config/.config/nvim/lua/mappings.lua
+++ modules/nvim/config/.config/nvim/lua/mappings.lua
@@ -6,3 +6,11 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- gitsigns
+map("n", "]c", function() require("gitsigns").nav_hunk("next") end, { desc = "gitsigns next hunk" })
+map("n", "[c", function() require("gitsigns").nav_hunk("prev") end, { desc = "gitsigns prev hunk" })
+map("n", "<leader>hs", function() require("gitsigns").stage_hunk() end, { desc = "gitsigns stage hunk" })
+map("n", "<leader>hr", function() require("gitsigns").reset_hunk() end, { desc = "gitsigns reset hunk" })
+map("n", "<leader>hp", function() require("gitsigns").preview_hunk() end, { desc = "gitsigns preview hunk" })
+map("n", "<leader>hb", function() require("gitsigns").blame_line({ full = true }) end, { desc = "gitsigns blame line" })
